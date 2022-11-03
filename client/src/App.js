0af8bfade33c397d134a04ba4d3bfc1d6986620d
Abhinav <raghunandan.abhinav@gmail.com>
2022-11-03 12:14:55 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+import sharelife from './images/sharelife.jpg'
+const App = ()=> {
+    return (
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">
+                    ShareLife
+                </Typography>
+                <img src= {sharelife} alt='sharelife' height="500" />            
+                </AppBar>
+                <Grow in>
+                    <Container>
+                        <Grid container justify="space-between" alignItems="stretch" spacing="3">
+                            <Grid item xs={12} sm={7}>
+                                <Posts />
+                            </Grid>
+                            <Grid item xs={12} sm={4}>
+                                <Form />   
+                            </Grid>
+                        </Grid>
+                    </Container>
+
+                </Grow>
+        </Container>
+    )
+}
+
+export default App;
